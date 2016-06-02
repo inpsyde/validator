b560c3100538d32d102b356df7e7bddac19a99f8
ChriCo <family.brueckner@googlemail.com>
2016-06-02 17:49:27 +0200

    improved doc-block on fake validator

--- tests/phpunit/Unit/Fake/AlwaysFalseWithInvalidMessageValidator.php
+++ tests/phpunit/Unit/Fake/AlwaysFalseWithInvalidMessageValidator.php
@@ -4,6 +4,13 @@ namespace Inpsyde\Validator\Tests\Unit\Fake;
 
 use Inpsyde\Validator\AbstractValidator;
 
+/**
+ * Class AlwaysFalseWithInvalidMessageValidator
+ *
+ * This class is just a simple "Fake" which is only used in tests to check, if the $options and error messages can be overwritten.
+ * 
+ * @package Inpsyde\Validator\Tests\Unit\Fake
+ */
 class AlwaysFalseWithInvalidMessageValidator extends AbstractValidator {
 
 	const INVALID = 'invalid';
